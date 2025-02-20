8ec05a2c6a85c84e5e00f06eaafe9c5374474c7f
katerinex <145131693+katerinex@users.noreply.github.com>
2025-02-19 20:48:17 -0500

    commit

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,4 +1,5 @@
 //src/Labs/Lab3/index.tsx
+import Add from "./Add";
 import VariablesAndConstants from "./VariablesAndConstants";
 
 export default function Lab3() {
@@ -6,6 +7,7 @@ export default function Lab3() {
     <div id="wd-lab3">
       <h3>Lab 3</h3>
       <VariablesAndConstants/>
+      <Add a={3} b={4} />
     </div>
   );
 }
