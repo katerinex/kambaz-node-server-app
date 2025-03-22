aba4d5610664a27aea0d62efa8bdcad5f83f7f94
katerinex <145131693+katerinex@users.noreply.github.com>
2025-03-22 12:32:57 -0400

    commit

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: process.env.NETLIFY_URL || "http://localhost:3000",
+    origin: process.env.NETLIFY_URL || "http://localhost:5173",
   })
 );
 app.use(express.json());
